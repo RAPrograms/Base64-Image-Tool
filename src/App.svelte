9b281304c0331792a49e689415c7506c7b0dbeb5
Dog2657 <RAPrograms@outlook.com>
2025-09-08 15:11:34 +0100

    Created Header

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,8 @@
+<script>
+    import Header from "./components/Header.svelte";
+
+</script>
+
+<Header/>
+
 <h1>Hello World</h1>
---
+++ src/components/Header.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    const owner = import.meta.env.VITE_PROJECT_OWNER
+</script>
+
+<header>
+    <h1>Base64 Image Tool</h1>
+    <div>By {owner}</div>
+</header>
+
+<style lang="scss">
+    header{
+        padding: 10px;
+
+        & > h1{
+            line-height: 20px;
+            font-size: x-large;
+        }
+
+        & > div{
+            font-style: italic;
+            font-size: small;
+        }
+    }
+</style>
